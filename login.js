05aff676c561caf56b3d881d1a3955cee6cf9271
helix693 <adhishan212@gmail.com>
2025-12-21 11:26:16 -0500

    removed password

--- login.js
+++ login.js
@@ -1,20 +1,9 @@
-function login() {
-    let input = prompt("Enter Password:");
-    if (input == "177>176lol") {
-        return;
-    } else {
-        alert("Incorrect Password!!!!!!!!!!! >:(");
-        login();
-    }
-}
-
 // Check if the user is already signed in
 const lastSignIn = localStorage.getItem('lastSignIn');
 if (lastSignIn && new Date(lastSignIn).getDate() === new Date().getDate()) {
   // User has already signed in today, do nothing
 } else {
-  // User has not signed in today, show prompt
-  login();
+  // User has not signed in today
   // Save sign-in status and current date to localStorage
   localStorage.setItem('lastSignIn', new Date().toISOString());
 }
