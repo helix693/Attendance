f68af4ab4ffbe0648741600a110eb93122dcc73e
helix693 <adhishan212@gmail.com>
2025-12-21 11:34:58 -0500

    its back!

--- login.js
+++ login.js
@@ -1,9 +1,20 @@
+function login() {
+    let input = prompt("Enter Password:");
+    if (input == "177>176lol") {
+        return;
+    } else {
+        alert("Incorrect Password!!!!!!!!!!! >:(");
+        login();
+    }
+}
+
 // Check if the user is already signed in
 const lastSignIn = localStorage.getItem('lastSignIn');
 if (lastSignIn && new Date(lastSignIn).getDate() === new Date().getDate()) {
   // User has already signed in today, do nothing
 } else {
-  // User has not signed in today
+  // User has not signed in today, show prompt
+  login();
   // Save sign-in status and current date to localStorage
   localStorage.setItem('lastSignIn', new Date().toISOString());
 }
